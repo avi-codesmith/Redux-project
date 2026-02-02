bac115499ea8e186a82e5430c1f5d34eb128504f
avi-codesmith <avi.codesmith@gmail.com>
2026-02-02 23:27:22 +0530

    Add redux reducer

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+// const { redux } = "redux"; wrong
+// const redux = require("redux"); or
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// }); // subscribe like that
+
+// store.dispatch({ type: "increment" });
+// store.dispatch({ type: "decrement" });
+
+export default store;
